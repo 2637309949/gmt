93b10c18b38b5ad25d9ddd5e357b9ed28b8d7920
2637309949 <2637309949@qq.com>
2021-09-14 11:38:37 +0800

    fix: .env

--- script/install.sh
+++ script/install.sh
@@ -19,9 +19,12 @@ unzip $tempdir/protoc/protoc-3.10.0-linux-x86_64.zip -d $tempdir/protoc/protoc-3
 cp $tempdir/protoc/protoc-3.10.0-linux-x86_64/bin/protoc $GOPATH/bin 
 
 # install micro
-git clone https://hub.fastgit.org/micro/micro.git $tempdir/micro
+git clone --branch v2.9.3 https://hub.fastgit.org/micro/micro.git $tempdir/micro
+git clone https://hub.fastgit.org/2637309949/gmt.git $tempdir/gmt
 cd $tempdir/micro
-git checkout v2.9.3
+cp -rf ../gmt/comm ../gmt/script/.env . 
+rm comm/go.mod comm/go.sum 
+sed -i "s/comm\//github.com\/micro\/micro\/v2\/comm\//g" `find comm -name "*.go" | xargs grep "comm\/" -rl`
 cat > main.go <<EOF
 package main
 
@@ -39,30 +42,55 @@ RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositorie
 RUN apk --no-cache add git gcc libtool musl-dev
 COPY . /
 WORKDIR /
-RUN git clone https://github.com/2637309949/gmt.git
-RUN cp -rf gmt/comm / && rm comm/go.mod comm/go.sum
-RUN sed -i "s/comm\//github.com\/micro\/micro\/v2\/comm\//g" `find comm -name "*.go" | xargs grep "comm\/" -rl`
 RUN go env -w GOPROXY=https://goproxy.cn,direct
 RUN go build -a -installsuffix cgo -ldflags "-s -w -X github.com/micro/micro/v2/cmd.GitCommit=e60b2cac -X github.com/micro/micro/v2/cmd.GitTag=v2.9.3 -X github.com/micro/micro/v2/cmd.BuildDate=1631063201" -o micro
 ENTRYPOINT ["/micro"]
 EOF
 
 # build docker 
-git clone https://github.com/2637309949/gmt.git $tempdir/gmt
-cp -rf $tempdir/gmt/comm $tempdir/micro
 go env -w GOPROXY=https://goproxy.cn,direct
 go build -a -installsuffix cgo -ldflags "-s -w -X github.com/micro/micro/v2/cmd.GitCommit=e60b2cac -X github.com/micro/micro/v2/cmd.GitTag=v2.9.3 -X github.com/micro/micro/v2/cmd.BuildDate=1631063201" -o micro
 cp micro $GOPATH/bin
 docker build -t micro:2.9.3 .
-
-# clean tmp
-rm -rf $tempdir
-
-# booting micro
-docker run --env-file=.env -d --name=micro -p 8080:8080  micro:2.9.3 api 
-docker run --env-file=.env micro:2.9.3 list services
+docker pull consul:1.8.5
 
 # booting consul
-mkdir -p /etc/consul.d
-mkdir -p /data/consul
-docker run --name=consul -d -p 8500:8500 -v /etc/consul.d/:/etc/consul.d/ -v /data/consul:/data/consul consul:1.8.5 agent -server -ui -config-dir /etc/consul.d/ -data-dir=/data/consul -log-file=/data/consul/log
+sudo mkdir -p /etc/consul.d
+sudo mkdir -p /data/consul
+sudo chmod 777 /data/consul
+sudo chmod 777 /etc/consul.d
+if [ ! "$(docker ps -q -f name=consul)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=consul)" ]; then
+        docker rm consul
+    fi
+	docker run --name=consul -d -p 8500:8500 -v /etc/consul.d/:/etc/consul.d/ -v /data/consul:/data/consul consul:1.8.5 agent -server -ui -config-dir /etc/consul.d/ -data-dir=/data/consul -log-file=/data/consul/log
+fi
+
+# booting micro
+if [ ! -f /etc/micro.d/.env ]; then
+sudo mkdir -p /etc/micro.d/
+sudo touch /etc/micro.d/.env
+sudo chmod 777 /etc/micro.d/.env
+sudo cat > /etc/micro.d/.env <<EOF
+MICRO_REGISTRY=consul
+MICRO_API_NAMESPACE=go.micro
+MICRO_API_HANDLER=api
+MICRO_API_ADDRESS=0.0.0.0:8080
+MICRO_REGISTRY_ADDRESS=172.30.13.77:8500
+MICRO_REGISTER_TTL=10
+MICRO_REGISTER_INTERVAL=5
+EOF
+fi
+
+if [ ! "$(docker ps -q -f name=micro)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=micro)" ]; then
+        docker rm micro
+    fi
+    docker run --env-file=/etc/micro.d/.env -d --name=micro -p 8080:8080  micro:2.9.3 api
+fi
+
+docker run --rm --env-file=/etc/micro.d/.env micro:2.9.3 list services
+
+# clean tmp
+cd $pwd
+rm -rf $tempdir

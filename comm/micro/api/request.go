464267b9809caa8337f652eefb88e8b53804ec62
2637309949 <2637309949@qq.com>
2021-09-22 15:45:04 +0800

    style: 调整proto

--- comm/micro/api/request.go
+++ comm/micro/api/request.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"comm/micro/bind"
+	"reflect"
+	"sync"
 
 	go_api "github.com/micro/go-micro/v2/api/proto"
 )
@@ -9,18 +11,61 @@ import (
 // Request defined TODO
 type Request struct {
 	*go_api.Request
-	User string
+	Keys map[string]interface{}
+	mu   sync.RWMutex
 }
 
+// Bind defined TODO
 func (r *Request) Bind(i interface{}) error {
-	return bind.Bind(i, r.Request)
+	err := bind.Bind(i, r.Request)
+	if err != nil {
+		return err
+	}
+	iv := reflect.ValueOf(i)
+	iv = reflect.Indirect(iv)
+	xi := iv.FieldByName("XUser")
+	if xi.CanSet() && xi.IsValid() && xi.Type().Kind() == reflect.String {
+		xUser, _ := r.Get("X-User")
+		xi.Set(reflect.ValueOf(xUser))
+	}
+	for k, v := range r.Keys {
+		if k == "X-User" {
+			continue
+		}
+		xi := iv.FieldByName(k)
+		if xi.CanSet() && xi.IsValid() && xi.IsZero() && xi.Type().Kind() == reflect.TypeOf(v).Kind() {
+			xi.Set(reflect.ValueOf(v))
+		}
+	}
+	return nil
 }
 
+// Get returns the value for the given key, ie: (value, true).
+// If the value does not exists it returns (nil, false)
+func (r *Request) Get(key string) (value interface{}, exists bool) {
+	r.mu.RLock()
+	value, exists = r.Keys[key]
+	r.mu.RUnlock()
+	return
+}
+
+// Set is used to store a new key/value pair exclusively for this context.
+// It also lazy initializes  c.Keys if it was not used previously.
+func (r *Request) Set(key string, value interface{}) {
+	r.mu.Lock()
+	if r.Keys == nil {
+		r.Keys = make(map[string]interface{})
+	}
+	r.Keys[key] = value
+	r.mu.Unlock()
+}
+
+// NewRequest defined TODO
 func NewRequest(r *go_api.Request) *Request {
 	rr := Request{Request: r}
-	xUser := r.GetHeader()["x-user"]
+	xUser := r.GetHeader()["X-User"]
 	if len(xUser.GetValues()) > 0 {
-		rr.User = xUser.GetValues()[0]
+		rr.Set("X-User", xUser.GetValues()[0])
 	}
 	return &rr
 }
--- helloworld-service/srv/handler/helloworld_dbhandler.go
+++ helloworld-service/srv/handler/helloworld_dbhandler.go
@@ -12,6 +12,7 @@ import (
 
 func (h *Handler) ArticleAddDB(ctx context.Context, db *xorm.Engine, data *types.Article) error {
 	logger.Info("Received ArticleAddDB request")
+
 	ret, err := db.Insert(data)
 	if err != nil {
 		return err
@@ -22,6 +23,7 @@ func (h *Handler) ArticleAddDB(ctx context.Context, db *xorm.Engine, data *types
 
 func (h *Handler) ArticleDelDB(ctx context.Context, db *xorm.Engine, filter *types.Article) error {
 	logger.Info("Received ArticleDelDB request")
+
 	ret, err := db.ID(filter.ID.Int64).Update(&types.Article{
 		UpdateTime: null.TimeFrom(time.Now()),
 		IsDelete:   null.IntFrom(1),
@@ -35,6 +37,7 @@ func (h *Handler) ArticleDelDB(ctx context.Context, db *xorm.Engine, filter *typ
 
 func (h *Handler) ArticleUpdateDB(ctx context.Context, db *xorm.Engine, data *types.Article) error {
 	logger.Info("Received ArticleUpdateDB request")
+
 	ret, err := db.ID(data.ID.Int64).Update(data)
 	if err != nil {
 		return err
@@ -45,6 +48,7 @@ func (h *Handler) ArticleUpdateDB(ctx context.Context, db *xorm.Engine, data *ty
 
 func (h *Handler) ArticleOneDB(ctx context.Context, db *xorm.Engine, filter *types.Article, data *types.Article) error {
 	logger.Info("Received ArticleOneDB request")
+
 	_, err := db.Get(data)
 	if err != nil {
 		return err
@@ -54,6 +58,7 @@ func (h *Handler) ArticleOneDB(ctx context.Context, db *xorm.Engine, filter *typ
 
 func (h *Handler) ArticlePageDB(ctx context.Context, db *xorm.Engine, filter *types.Article, list *[]types.Article, totalRecord ...*int64) error {
 	logger.Info("Received ArticlePageDB request")
+
 	err := db.Find(list, filter)
 	if err != nil {
 		return err
